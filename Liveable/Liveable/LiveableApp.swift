3664d9ebda7d02e075860f2b7ad0dc9bbe24ca56
Engin Gülek <engingulek0@gmail.com>
2023-09-24 19:26:49 +0300

    Creat Project

---
+++ Liveable/Liveable/LiveableApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveableApp.swift
+//  Liveable
+//
+//  Created by engin gülek on 24.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
